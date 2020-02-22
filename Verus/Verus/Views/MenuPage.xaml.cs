48bb92c25a88695076ed6253d3c374cf10f49627
jarlene <xwl198937@hotmail.com>
2020-02-22 08:24:22 +0800

    first commit

---
+++ Verus/Verus/Views/MenuPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Verus.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Verus.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MenuPage : ContentPage
+    {
+        MainPage RootPage { get => Application.Current.MainPage as MainPage; }
+        List<HomeMenuItem> menuItems;
+        public MenuPage()
+        {
+            InitializeComponent();
+
+            menuItems = new List<HomeMenuItem>
+            {
+                new HomeMenuItem {Id = MenuItemType.Browse, Title="Browse" },
+                new HomeMenuItem {Id = MenuItemType.About, Title="About" }
+            };
+
+            ListViewMenu.ItemsSource = menuItems;
+
+            ListViewMenu.SelectedItem = menuItems[0];
+            EventHandler<SelectedItemChangedEventArgs> p = async (sender, e) =>
+               {
+                   if (e.SelectedItem == null)
+                       return;
+
+                   var id = (int)((HomeMenuItem)e.SelectedItem).Id;
+                   await RootPage.NavigateFromMenu(id);
+               };
+            ListViewMenu.ItemSelected += p;
+        }
+    }
+}
